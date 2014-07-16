1396cdfcba5fce8749d6fc38a7fef920dcf44402
nobleclem <nobleclem@fatalexception.us>
2014-07-16 13:39:50 -0400

    Update wp-enhancements.php
    add new filter and tweak existing url filter

--- wp-enhancements.php
+++ wp-enhancements.php
@@ -111,16 +111,39 @@ add_filter( 'widget_bu_pages_args', 'wpenhancements_widget_bu_pages_args', 9999 
 
 
 /* WPMU Domain Mapping Tweaks */
-function wpenhancements_wp_nav_menu( $nav_menu, $args )
+function wpenchancements_wp_get_nav_menu_items( $items )
 {
     if( defined( 'DOMAIN_MAPPING' ) && function_exists( 'get_original_url' ) ) {
-        $nav_menu = str_replace( 
-            get_original_url( 'siteurl' ),
-            get_option( 'siteurl' ), 
-            $nav_menu
+        foreach( $items as &$item ) {
+            $item->url = str_replace(
+                preg_replace( '#^https?://#i', '', get_original_url( 'siteurl' ) ),
+                preg_replace( '#^https?://#i', '', get_option( 'siteurl' ) ),
+                $item->url
+            );
+
+            $item->guid = str_replace(
+                preg_replace( '#^https?://#i', '', get_original_url( 'siteurl' ) ),
+                preg_replace( '#^https?://#i', '', get_option( 'siteurl' ) ),
+                $item->guid
+            );
+        }
+    }
+
+    return $items;
+}
+add_filter( 'wp_get_nav_menu_items', 'wpenchancements_wp_get_nav_menu_items', 10 );
+
+function wpenchancements_string_url_fix( $string )
+{
+    if( defined( 'DOMAIN_MAPPING' ) && function_exists( 'get_original_url' ) ) {
+        $string = str_replace(
+            preg_replace( '#^https?://#i', '', get_original_url( 'siteurl' ) ),
+            preg_replace( '#^https?://#i', '', get_option( 'siteurl' ) ),
+            $string
         );
     }
 
-    return $nav_menu;
+    return $string;
 }
-add_filter( 'wp_nav_menu', 'wpenhancements_wp_nav_menu', 10, 2 );
+add_filter( 'theme_mod_header_image', 'wpenchancements_string_url_fix' );
+add_filter( 'the_content', 'wpenchancements_string_url_fix' );

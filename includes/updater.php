8b09632e812beea0d5b6c746a29440b5278392ba
nobleclem <nobleclem@fatalexception.us>
2014-10-27 14:11:57 -0400

    Update updater.php

--- includes/updater.php
+++ includes/updater.php
@@ -242,7 +242,7 @@ class WP_GitHub_Updater {
 			if ( is_wp_error( $raw_response ) )
 				return $version;
 
-			preg_match( '/^[ \t\/*#@]*~Current Version:(.*)~$/mi', $raw_response['body'], $__version );
+			preg_match( '#^\s*`*~Current Version\:\s*([^~]*)~#im', $raw_response['body'], $__version );
 
 			if ( isset( $__version[1] ) ) {
 				$version_readme = $__version[1];
